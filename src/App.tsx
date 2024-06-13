315b26649f554b13456044a33984d9b2be7bc0f9
Yulia Hernandez <iuliiahernandez2109@gmail.com>
2024-06-13 16:03:15 -0500

    added edit improvement form and images

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import ResourcesView from "./components/ResourcesView";
 import Map from "./components/Map";
 import Resource from "./models/Resource";
 import Improvement from "./models/Improvement";
-import AddImprovementDialog from "./components/AddImprovementDialog";
-import EditImprovementDialog from "./components/EditImprovementDialog";
 import { Type } from "./models/ImprovementsArray";
 
 function App() {
@@ -21,65 +19,93 @@ function App() {
 
   const [improvements, setImprovements] = useState<Improvement[]>(
     Array.apply(null, Array(25)).map((el, i) => {
-      return {_id: i, type: "empty", level: 0 };
+      return { _id: i, type: "empty", level: 0 };
     })
   );
 
   const addImprovement = (index: number, improvement: Type): void => {
-    setImprovements([...improvements.slice(0, index), {_id: index, type: improvement.type, level: 1},...improvements.slice(index+1)])
-    if(improvement.benefit.resource.toLocaleLowerCase() === "people") {
-      setPeople({name:people.name, amount: people.amount + improvement.benefit.amount});
-    }else if(improvement.benefit.resource.toLocaleLowerCase() === "papyrus") {
-      setPapyrus({name:papyrus.name, amount: papyrus.amount + improvement.benefit.amount});
-    }else if(improvement.benefit.resource.toLocaleLowerCase() === "fish") {
-      setFish({name:fish.name, amount: fish.amount + improvement.benefit.amount});
-    }else if(improvement.benefit.resource.toLocaleLowerCase() === "bricks") {
-      setBricks({name:bricks.name, amount: bricks.amount + improvement.benefit.amount});
-    }else if(improvement.benefit.resource.toLocaleLowerCase() === "water") {
-      setWater({name:water.name, amount: water.amount + improvement.benefit.amount});
+    setImprovements([
+      ...improvements.slice(0, index),
+      { _id: index, type: improvement.type, level: 1 },
+      ...improvements.slice(index + 1),
+    ]);
+    if (improvement.benefit.resource.toLocaleLowerCase() === "people") {
+      setPeople({
+        name: people.name,
+        amount: people.amount + improvement.benefit.amount,
+      });
+    } else if (improvement.benefit.resource.toLocaleLowerCase() === "papyrus") {
+      setPapyrus({
+        name: papyrus.name,
+        amount: papyrus.amount + improvement.benefit.amount,
+      });
+    } else if (improvement.benefit.resource.toLocaleLowerCase() === "fish") {
+      setFish({
+        name: fish.name,
+        amount: fish.amount + improvement.benefit.amount,
+      });
+    } else if (improvement.benefit.resource.toLocaleLowerCase() === "bricks") {
+      setBricks({
+        name: bricks.name,
+        amount: bricks.amount + improvement.benefit.amount,
+      });
+    } else if (improvement.benefit.resource.toLocaleLowerCase() === "water") {
+      setWater({
+        name: water.name,
+        amount: water.amount + improvement.benefit.amount,
+      });
     }
     improvement.cost.forEach((c) => {
-      if(c.resource.toLocaleLowerCase() === "people") {
-        setPeople({name:people.name, amount: people.amount - c.amount});
-      }else if(c.resource.toLocaleLowerCase() === "papyrus") {
-        setPapyrus({name:papyrus.name, amount: papyrus.amount - c.amount});
-      }else if(c.resource.toLocaleLowerCase() === "fish") {
-        setFish({name:fish.name, amount: fish.amount - c.amount});
-      }else if(c.resource.toLocaleLowerCase() === "bricks") {
-        setBricks({name:bricks.name, amount: bricks.amount - c.amount});
-      }else if(c.resource.toLocaleLowerCase() === "water") {
-        setWater({name:water.name, amount: water.amount - c.amount});
+      if (c.resource.toLocaleLowerCase() === "people") {
+        setPeople({ name: people.name, amount: people.amount - c.amount });
+      } else if (c.resource.toLocaleLowerCase() === "papyrus") {
+        setPapyrus({ name: papyrus.name, amount: papyrus.amount - c.amount });
+      } else if (c.resource.toLocaleLowerCase() === "fish") {
+        setFish({ name: fish.name, amount: fish.amount - c.amount });
+      } else if (c.resource.toLocaleLowerCase() === "bricks") {
+        setBricks({ name: bricks.name, amount: bricks.amount - c.amount });
+      } else if (c.resource.toLocaleLowerCase() === "water") {
+        setWater({ name: water.name, amount: water.amount - c.amount });
       }
-    })
-  }
+    });
+  };
 
   const checkImprovement = (improvement: Type): boolean => {
-    if(improvement.cost.find((c) => {
-      if(c.resource.toLocaleLowerCase() === "people") {
-        return c.amount > people.amount;
-      }else if(c.resource.toLocaleLowerCase() === "papyrus") {
-        return c.amount > papyrus.amount;
-      }else if(c.resource.toLocaleLowerCase() === "fish") {
-        return c.amount > fish.amount;
-      }else if(c.resource.toLocaleLowerCase() === "bricks") {
-        return c.amount > bricks.amount;
-      }else if(c.resource.toLocaleLowerCase() === "water") {
-        return c.amount > water.amount;
-      }
-    })) {
+    if (
+      improvement.cost.find((c) => {
+        if (c.resource.toLocaleLowerCase() === "people") {
+          return c.amount > people.amount;
+        } else if (c.resource.toLocaleLowerCase() === "papyrus") {
+          return c.amount > papyrus.amount;
+        } else if (c.resource.toLocaleLowerCase() === "fish") {
+          return c.amount > fish.amount;
+        } else if (c.resource.toLocaleLowerCase() === "bricks") {
+          return c.amount > bricks.amount;
+        } else if (c.resource.toLocaleLowerCase() === "water") {
+          return c.amount > water.amount;
+        }
+      })
+    ) {
       // console.log(improvement)
       return true;
     }
     return false;
-  }
+  };
 
-  const editImprovement = (index: number, improvement: Type, action: string): void => {
-
-  }
+  const editImprovement = (
+    index: number,
+    improvement: Type,
+    action: string
+  ): void => {};
 
   return (
     <>
-      <Map improvements={improvements} addImprovement={addImprovement} editImprovement={editImprovement} checkImprovement={checkImprovement}/>
+      <Map
+        improvements={improvements}
+        addImprovement={addImprovement}
+        editImprovement={editImprovement}
+        checkImprovement={checkImprovement}
+      />
       <ResourcesView
         people={people}
         papyrus={papyrus}
@@ -87,8 +113,6 @@ function App() {
         bricks={bricks}
         water={water}
       />
-      {/* <AddImprovementDialog />
-      <EditImprovementDialog /> */}
     </>
   );
 }
--- src/components/EditImprovementDialog.tsx
+++ src/components/EditImprovementDialog.tsx
@@ -1,24 +1,69 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import "./EditImprovementDialog.css";
-import { Type } from "../models/ImprovementsArray";
+import { ImprovementsArray, Type } from "../models/ImprovementsArray";
+import Improvement from "../models/Improvement";
 
 interface Props {
-  index: number;
+  improvement: Improvement;
   endTurn(): void;
   hideDialog(): void;
   editImprovement(index: number, improvement: Type, action: string): void;
   checkImprovement(improvement: Type): boolean;
 }
 
-const EditImprovementDialog = ({index, endTurn, hideDialog, editImprovement, checkImprovement}: Props) => {
+const EditImprovementDialog = ({
+  improvement,
+  endTurn,
+  hideDialog,
+  editImprovement,
+  checkImprovement,
+}: Props) => {
   const submitListener = (e: FormEvent) => {
     e.preventDefault();
     endTurn();
     hideDialog();
-  }
+  };
+
+  const element: Type | undefined = ImprovementsArray.find((item) => {
+    return item.type === improvement.type;
+  });
 
   return (
-    <div className="EditImprovementDialog">EditImprovementDialog works</div>
+    <form className="EditImprovementDialog">
+      <label htmlFor="type">
+        Type: <p>{element?.type}</p>
+      </label>
+      <label htmlFor="level">
+        Level: <p>{improvement?.level}</p>
+      </label>
+      <label htmlFor="benefit">
+        Benefit:{" "}
+        <p>
+          {element?.benefit.amount} {element?.benefit.resource}
+        </p>
+      </label>
+      <label htmlFor="cost">
+        Cost:{" "}
+        {element?.cost.map((item) => (
+          <p>
+            {item.amount} {item.resource}
+          </p>
+        ))}
+      </label>
+      <button
+        type="button"
+        id="cancel"
+        onClick={() => {
+          endTurn();
+          hideDialog();
+        }}
+      >
+        Cancel
+      </button>
+      <button>Upgrade</button>
+      <button>Downgrade</button>
+      <button>Remove</button>
+    </form>
   );
 };
 
